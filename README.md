4f9c50a1934b3263252badae3da21c2e15ac46fb
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-02-20 19:23:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# git-pull-all
+
+Asynchronous git-pull(1) for multiple Git projects.
+
+## Usage
+
+```
+$ cd ~/Projects
+$ ls
+git-project-a  git-project-b  git-project-c
+$ ./git-pull-all
+/home/tatsuyaoiw/Projects/Github/git-project-a/
+Already up-to-date.
+
+/home/tatsuyaoiw/Projects/Github/git-project-b/
+Already up-to-date.
+
+/home/tatsuyaoiw/Projects/Github/git-project-c/
+Already up-to-date.
+```
+
+## Licence
+
+MIT
---
+++ index.js
@@ -0,0 +1,135 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+var join = require('path').join;
+
+var async = require('async');
+
+/**
+ * Return the child directories of the given parent directory.
+ *
+ * @param  {String}   parent
+ * @param  {Function} callback
+ */
+function readdirs(parent, callback) {
+  fs.readdir(parent, function(err, children) {
+    if (err) {
+      return callback(err);
+    }
+
+    var files = children.map(function(child) {
+      return join(parent, child);
+    });
+
+    async.filter(files, isGitProject, function(dirs) {
+      callback(null, dirs);
+    });
+  })
+}
+
+/**
+ * Check if file is a directory.
+ *
+ * @param  {String}   file
+ * @param  {Function} callback
+ */
+function isGitProject(file, callback) {
+  fs.stat(file, function(err, stats) {
+    if (err) {
+      console.log(err);
+      return callback(false);
+    }
+    if (!stats.isDirectory()) {
+      return callback(false);
+    }
+    fs.exists(join(file, '.git'), callback);
+  });
+}
+
+/**
+ * Run the given command.
+ *
+ * @param  {String} command
+ * @param  {Object} options
+ */
+function run(command, options, callback) {
+  options = options || {};
+  var child = exec(command, options, callback);
+}
+
+function gitPull(dir, callback) {
+  gitRemoteShow(dir, function(err, result) {
+    if (err) {
+      return callback(err);
+    }
+    if (!result.stdout) {
+      return callback(null, {
+        dir: dir,
+        stdout: 'Ignored because remote repository does not exist\n'
+      });
+    }
+    var command = 'git pull';
+    run(command, { cwd: dir }, function(err, stdout, stderr) {
+      if (err) {
+        return callback(wrapError(dir, command, err));
+      }
+      if (stderr) {
+        return callback(wraperror(dir, command, stderr));
+      }
+      var ret = {
+        dir: dir,
+        stdout: stdout
+      };
+      callback(null, ret);
+    });
+  });
+}
+
+function gitRemoteShow(dir, callback) {
+  var command = 'git remote show';
+  run(command, { cwd: dir }, function(err, stdout, stderr) {
+    if (err) {
+      return callback(wrapError(dir, command, err));
+    }
+    if (stderr) {
+      return callback(wraperror(dir, command, stderr));
+    }
+    var ret = {
+      dir: dir,
+      stdout: stdout
+    };
+    callback(null, ret);
+  });
+}
+
+function wrapError(dir, command, err) {
+  var message = [
+    'Something went wrong on "' + dir + '" ...\n',
+    'Command: ' + command,
+    'Message: ' + err.message
+  ].join('');
+  return new Error(message);
+}
+
+// Parse command line arguments
+var argv = process.argv.slice(2);
+var cwd = join(process.cwd(), argv.shift() || '.');
+
+readdirs(cwd, function(err, dirs) {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+
+  async.map(dirs, gitPull, function(err, results) {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    results.forEach(function(result) {
+      console.log('\033[36m' + result.dir + '/\033[39m');
+      if (result.stdout) {
+        console.log(result.stdout);
+      }
+    });
+  });
+});
