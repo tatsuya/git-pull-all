371bee9174a648b176de678f87df2bab48ac0919
Shashank Singal <shashank.singal@go-jek.com>
2019-01-21 15:35:53 +0530

    enable recursive git-pull

--- cli.js
+++ cli.js
@@ -2,8 +2,15 @@
 
 var path = require('path');
 var gitPullAll = require('.');
+var cwd = process.cwd();
 
 // Parse command line arguments
 var argv = process.argv.slice(2);
-var parentDir = path.join(process.cwd(), argv.shift() || '.');
-gitPullAll(parentDir);
+var isRecursive = argv.includes('-r');
+var paramOne = argv.shift();
+var parentDir = cwd;
+
+if (paramOne && paramOne != '-r') {
+  parentDir = path.join(cwd, paramOne);
+}
+gitPullAll(parentDir, isRecursive);
